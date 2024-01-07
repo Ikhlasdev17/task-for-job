1f031c7db75ae3a9497f9c71e949c36746201eee
Ikhlasdeveloper77 <xeneje8700@apdiv.com>
2024-01-08 02:21:14 +0500

    some bugs fixed

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,14 +1,19 @@
 import { ChildProps } from '@/types/types'
+import { Suspense } from 'react'
 import { Navbar, Sidebar } from './components'
 export default function HomeLayout({ children }: ChildProps) {
 	return (
-		<div className='h-full'>
-			<Navbar />
+		<Suspense fallback={<p>Loading...</p>}>
+			<div className='h-full'>
+				<Navbar />
 
-			<section className='h-full p-4 flex gap-4 sm:flex-col lg:flex-row'>
-				<Sidebar />
-				<main className='h-full border p-4 rounded-md w-full'>{children}</main>
-			</section>
-		</div>
+				<section className='h-full p-4 flex gap-4 sm:flex-col lg:flex-row'>
+					<Sidebar />
+					<main className='h-full border p-4 rounded-md w-full'>
+						{children}
+					</main>
+				</section>
+			</div>
+		</Suspense>
 	)
 }
